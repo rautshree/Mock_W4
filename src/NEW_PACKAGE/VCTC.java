2c960a965ee25163cb60bd88b568bd39657d41fb
DELL <DELL@DESKTOP-9S76OLD>
2022-03-29 20:36:24 +0530

    chnages done by me

--- src/NEW_PACKAGE/VCTC.java
+++ src/NEW_PACKAGE/VCTC.java
@@ -4,5 +4,6 @@ public class VCTC {
 public static void main(String[] args) {
 	System.out.println("VELOCITY TRAINING CENTRE");
 	System.out.println("hi iam vinayak");
+	System.out.println("hi iam patewar");
 }
 }
