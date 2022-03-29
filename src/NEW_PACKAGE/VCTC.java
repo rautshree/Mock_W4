cce19c1b5581db20c090f464ca888990bc1a6ba6
BHAGYASHREE RAUT <BHAGYASHREE RAUT@LAPTOP-DO61B2MO>
2022-03-29 20:07:41 +0530

    uPDATED1

--- src/NEW_PACKAGE/VCTC.java
+++ src/NEW_PACKAGE/VCTC.java
@@ -2,6 +2,6 @@ package NEW_PACKAGE;
 
 public class VCTC {
 public static void main(String[] args) {
-	System.out.println("VELOCITY TRAININT CENTRE");
+	System.out.println("VELOCITY TRAINING CENTRE");
 }
 }
